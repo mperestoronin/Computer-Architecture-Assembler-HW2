c25ddda9ffe34cd43c023a3c5638c1009e8cb7cc
Maxim Perestoronin <116491226+mperestoronin@users.noreply.github.com>
2022-11-12 17:37:57 +0300

    Create input_str.s

---
+++ Assembler_Original/input_str.s
@@ -0,0 +1,49 @@
+	.file	"input_str.c"
+	.intel_syntax noprefix
+	.text
+	.globl	input_str
+	.type	input_str, @function
+input_str:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	mov	rax, QWORD PTR stdin[rip]
+	mov	rdi, rax
+	call	fflush@PLT
+	mov	rax, QWORD PTR stdin[rip]
+	mov	rdx, rax
+	mov	esi, 256
+	lea	rdi, string.2495[rip]
+	call	fgets@PLT
+	lea	rax, string.2495[rip]
+	pop	rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	input_str, .-input_str
+	.local	string.2495
+	.comm	string.2495,256,32
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
