b6384d042b9dd3ccd23457e8bf2021d4e1699cda
Maxim Perestoronin <116491226+mperestoronin@users.noreply.github.com>
2022-11-13 00:43:20 +0300

    Update input_str_edited.s

--- Assembler_Edited/for_4_&_5_points/input_str_edited.s
+++ Assembler_Edited/for_4_&_5_points/input_str_edited.s
@@ -5,20 +5,23 @@
 	.type	input_str, @function
 input_str:
 	endbr64	
-	push	rbp	#
-	mov	rbp, rsp	#,
+	push	rbp
+	# начинаем передавать параметр в функцию fflush
+	mov	rbp, rsp
 # ./input_str.c:5:     fflush(stdin);
 	mov rdi, QWORD PTR stdin[rip]
-	call	fflush@PLT	#
+	# закончили передавать параметр в функцию fflush
+	call	fflush@PLT # вызов функции fflush
 # ./input_str.c:6:     fgets(string, 256, stdin);
-	mov rdx, QWORD PTR stdin[rip]
-	mov	esi, 256	#,
-	lea	rdi, string.2495[rip]	#,
-	call	fgets@PLT	#
+	# Начинаем передавать параметры в функцию fgets
+	mov rdx, QWORD PTR stdin[rip] # передаем stdin в функцию fgets
+	mov	esi, 256 # передаем 256
+	lea	rdi, string.2495[rip] # передаем string в fgets
+	call	fgets@PLT # вызов fgets
 # ./input_str.c:7:     return string;
-	lea	rax, string.2495[rip]	# _6,
+	lea	rax, string.2495[rip]
 # ./input_str.c:8: }
-	pop	rbp	#
+	pop	rbp
 	ret	
 	.size	input_str, .-input_str
 	.local	string.2495
