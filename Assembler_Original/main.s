fd0e4f1b25afa89d84e80306f83d3660e21ce3c6
Maxim Perestoronin <116491226+mperestoronin@users.noreply.github.com>
2022-11-12 17:35:56 +0300

    Create main.s

---
+++ Assembler_Original/main.s
@@ -0,0 +1,57 @@
+	.file	"main.c"
+	.intel_syntax noprefix
+	.text
+	.section	.rodata
+	.align 8
+.LC0:
+	.string	"Input your string (256 characters maximum) not extended ASCII only:"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	push	rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	mov	rbp, rsp
+	.cfi_def_cfa_register 6
+	sub	rsp, 16
+	lea	rdi, .LC0[rip]
+	call	puts@PLT
+	mov	eax, 0
+	call	input_str@PLT
+	mov	QWORD PTR -16[rbp], rax
+	mov	rax, QWORD PTR -16[rbp]
+	mov	rdi, rax
+	call	count@PLT
+	mov	QWORD PTR -8[rbp], rax
+	mov	rax, QWORD PTR -8[rbp]
+	mov	rdi, rax
+	call	output@PLT
+	mov	eax, 0
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
