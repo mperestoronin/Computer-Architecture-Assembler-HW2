024efac6d68d058974485c2e066b7350eb8d2c4d
Maxim Perestoronin <116491226+mperestoronin@users.noreply.github.com>
2022-11-13 00:11:23 +0300

    Update main_edited.s

--- Assembler_Edited/for_4_&_5_points/main_edited.s
+++ Assembler_Edited/for_4_&_5_points/main_edited.s
@@ -10,24 +10,24 @@
 	.type	main, @function
 main:
 	endbr64	
-	push	rbp	#
-	mov	rbp, rsp	#,
+	push	rbp	
+	mov	rbp, rsp	
 	sub	rsp, 16	#,
 # ./main.c:8:     printf("Input your string (256 characters maximum) not extended ASCII only:\n");
-	lea	rdi, .LC0[rip]	#,
-	call	puts@PLT	#
+	lea	rdi, .LC0[rip]	
+	call	puts@PLT	
 # ./main.c:9:     char* input = input_str();
 	mov	eax, 0	#,
-	call	input_str@PLT	#
+	call	input_str@PLT	
 	mov	QWORD PTR -8[rbp], rax	# input, tmp84 - записываем результат работы input_str в char* input
 # ./main.c:10:     int* result = count(input);
 	mov	rax, QWORD PTR -8[rbp]	# tmp85, input - передаем параметр input в функцию count
 	mov	rdi, rax	#, tmp85
-	call	count@PLT	#
+	call	count@PLT	
 	mov	QWORD PTR -16[rbp], rax	# result, tmp - записываем результат работы функции count в *result
 # ./main.c:11:     output(result);
 	mov rdi, QWORD PTR -16[rbp] # - передаем result в функцию output
-	call	output@PLT	#
+	call	output@PLT	
 	mov	eax, 0	# возвращаем 0 в конце успешной работы программы
 # ./main.c:12: }
 	leave	
