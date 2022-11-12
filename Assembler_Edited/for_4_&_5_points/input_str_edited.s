2b1eef75137f5e558fbb86f43aab2ee35a413500
Maxim Perestoronin <116491226+mperestoronin@users.noreply.github.com>
2022-11-12 20:13:16 +0300

    Update input_str_edited.s

--- Assembler_Edited/for_4_&_5_points/input_str_edited.s
+++ Assembler_Edited/for_4_&_5_points/input_str_edited.s
@@ -23,21 +23,3 @@ input_str:
 	.size	input_str, .-input_str
 	.local	string.2495
 	.comm	string.2495,256,32
-	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04.1) 9.4.0"
-	.section	.note.GNU-stack,"",@progbits
-	.section	.note.gnu.property,"a"
-	.align 8
-	.long	 1f - 0f
-	.long	 4f - 1f
-	.long	 5
-0:
-	.string	 "GNU"
-1:
-	.align 8
-	.long	 0xc0000002
-	.long	 3f - 2f
-2:
-	.long	 0x3
-3:
-	.align 8
-4:
