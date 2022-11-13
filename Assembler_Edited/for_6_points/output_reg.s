02d3d756939c037535b8d1130dfa976cd72eed4e
Maxim Perestoronin <116491226+mperestoronin@users.noreply.github.com>
2022-11-13 11:19:25 +0300

    Update output_reg.s

--- Assembler_Edited/for_6_points/output_reg.s
+++ Assembler_Edited/for_6_points/output_reg.s
@@ -19,12 +19,12 @@ output:
 	lea	rdi, .LC0[rip]	#,
 	call	puts@PLT	#
 # ./output.c:5:     for (int i = 33; i < 128; ++i) {
-	mov	r12d, 33	# i,
+	mov	r12d, 33	# i, (заменен DWORD PTR -4[rbp] (переменная i в С коде) на r12d)
 # ./output.c:5:     for (int i = 33; i < 128; ++i) {
 	jmp	.L2	#
 .L4:
 # ./output.c:6:         if (ascii[i] != 0) {
-	mov	eax, r12d	# tmp92, i
+	mov	eax, r12d	# tmp92, i (заменен DWORD PTR -4[rbp] (переменная i в С коде) на r12d)
 	lea	rdx, 0[0+rax*4]	# _2,
 	mov	rax, QWORD PTR -24[rbp]	# tmp93, ascii
 	add	rax, rdx	# _3, _2
@@ -33,14 +33,14 @@ output:
 	test	eax, eax	# _4
 	je	.L3	#,
 # ./output.c:7:             printf("%c - %i \n", (char)i, ascii[i]);
-	mov	eax, r12d	# tmp94, i
+	mov	eax, r12d	# tmp94, i (заменен DWORD PTR -4[rbp] (переменная i в С коде) на r12d)
 	lea	rdx, 0[0+rax*4]	# _6,
 	mov	rax, QWORD PTR -24[rbp]	# tmp95, ascii
 	add	rax, rdx	# _7, _6
 # ./output.c:7:             printf("%c - %i \n", (char)i, ascii[i]);
 	mov	edx, DWORD PTR [rax]	# _8, *_7
 # ./output.c:7:             printf("%c - %i \n", (char)i, ascii[i]);
-	mov	eax, r12d	# tmp96, i
+	mov	eax, r12d	# tmp96, i (заменен DWORD PTR -4[rbp] (переменная i в С коде) на r12d)
 # ./output.c:7:             printf("%c - %i \n", (char)i, ascii[i]);
 	movsx	eax, al	# _10, _9
 	mov	esi, eax	#, _10
@@ -49,10 +49,10 @@ output:
 	call	printf@PLT	#
 .L3:
 # ./output.c:5:     for (int i = 33; i < 128; ++i) {
-	add	r12d, 1	# i,
+	add	r12d, 1	# i, (заменен DWORD PTR -4[rbp] (переменная i в С коде) на r12d)
 .L2:
 # ./output.c:5:     for (int i = 33; i < 128; ++i) {
-	cmp	r12d, 127	# i,
+	cmp	r12d, 127	# i, (заменен DWORD PTR -4[rbp] (переменная i в С коде) на r12d)
 	jle	.L4	#,
 # ./output.c:10: }
 	nop	
